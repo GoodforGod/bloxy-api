54dc3cd74d7fe483b7e82bae997e5ad38043a46d
GoodforGod <goodforgod.dev@gmail.com>
2019-03-14 15:14:50 +0300

    Tests for dYdX and Livepeer set up

---
+++ src/test/kotlin/io/api/bloxy/core/dydx/WithdrawalsTests.kt
@@ -0,0 +1,12 @@
+package io.api.bloxy.core.dydx
+
+import io.api.bloxy.core.BloxyTester
+
+/**
+ * "default comment"
+ *
+ * @author GoodforGod
+ * @since 14.03.2019
+ */
+class WithdrawalsTests : BloxyTester() {
+}
---
+++ src/test/kotlin/io/api/bloxy/core/livepeer/RoundsTests.kt
@@ -0,0 +1,12 @@
+package io.api.bloxy.core.livepeer
+
+import io.api.bloxy.core.BloxyTester
+
+/**
+ * "default comment"
+ *
+ * @author GoodforGod
+ * @since 14.03.2019
+ */
+class RoundsTests : BloxyTester() {
+}
