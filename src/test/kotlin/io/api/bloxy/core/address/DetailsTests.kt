2842bc592a0a6a56ef31fc96dd2395ffb6a81f32
Anton Kurako <goodforgod.dev@gmail.com>
2019-09-14 21:02:01 +0300

    Minor test fixes

--- src/test/kotlin/io/api/bloxy/core/address/DetailsTests.kt
+++ src/test/kotlin/io/api/bloxy/core/address/DetailsTests.kt
@@ -23,7 +23,7 @@ class DetailsTests : BloxyTester() {
         mustValid(details[0].address)
         mayValid(details[0].annotation)
         mustValid(details[0].levelAsString)
-        mustValid(details[0].level)
+        mayValid(details[0].level)
         mayValid(details[0].note)
         mustValid(details[0].toString())
     }
